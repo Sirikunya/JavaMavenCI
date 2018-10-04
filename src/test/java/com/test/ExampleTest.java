aaa60ce26e1893890dfa5cbb1f99ab6b00220383
unknown <fill9029f@gmail.com>
2018-10-04 13:17:41 +0700

    init

---
+++ src/test/java/com/test/ExampleTest.java
@@ -0,0 +1,21 @@
+package com.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExampleTest {
+
+	@Test
+	public void test() {
+		//init value
+		int score = 8;
+		//expected
+		String expected ="A";
+		//actual
+		String actual ="B";
+		//test
+		Assert.assertEquals(expected, actual);
+		
+	}
+
+}
